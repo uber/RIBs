1a716b51e4ebf9425b3e8a27565af4f1c571bef1
Cameron <cameron@sbarow.com>
2017-10-25 22:47:57 +0200

    Remove tracking of pods

--- ios/tutorials/tutorial3-rib-di-and-communication/Pods/Target Support Files/RIBs/RIBs-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_RIBs : NSObject
-@end
-@implementation PodsDummy_RIBs
-@end
--- ios/tutorials/tutorial3-rib-di-and-communication/Pods/Target Support Files/SnapKit/SnapKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SnapKit : NSObject
-@end
-@implementation PodsDummy_SnapKit
-@end
