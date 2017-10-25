3e0c223b3e7146a0275f71e98bbee674e3d440cd
Pavel Mazurin <pavel@uber.com>
2017-10-25 21:05:31 +0200

    Tutorial assets

---
+++ tutorial_assets/ios/tutorial3-rib-di-and-communication/source/source1.swift
@@ -0,0 +1,63 @@
+//
+//  Copyright (c) 2017. Uber Technologies
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.yellow
+        buildStartButton()
+        buildPlayerLabels()
+    }
+
+    private func buildPlayerLabels() {
+        let labelBuilder: (UIColor, String) -> UILabel = { (color: UIColor, text: String) in
+            let label = UILabel()
+            label.font = UIFont.boldSystemFont(ofSize: 35)
+            label.backgroundColor = UIColor.clear
+            label.textColor = color
+            label.textAlignment = .center
+            label.text = text
+            return label
+        }
+
+        let player1Label = labelBuilder(UIColor.blue, player1Name)
+        view.addSubview(player1Label)
+        player1Label.snp.makeConstraints { (maker: ConstraintMaker) in
+            maker.top.equalTo(self.view).offset(70)
+            maker.leading.trailing.equalTo(self.view).inset(20)
+            maker.height.equalTo(40)
+        }
+
+        let vsLabel = UILabel()
+        vsLabel.font = UIFont.systemFont(ofSize: 25)
+        vsLabel.backgroundColor = UIColor.clear
+        vsLabel.textColor = UIColor.darkGray
+        vsLabel.textAlignment = .center
+        vsLabel.text = "vs"
+        view.addSubview(vsLabel)
+        vsLabel.snp.makeConstraints { (maker: ConstraintMaker) in
+            maker.top.equalTo(player1Label.snp.bottom).offset(10)
+            maker.leading.trailing.equalTo(player1Label)
+            maker.height.equalTo(20)
+        }
+
+        let player2Label = labelBuilder(UIColor.red, player2Name)
+        view.addSubview(player2Label)
+        player2Label.snp.makeConstraints { (maker: ConstraintMaker) in
+            maker.top.equalTo(vsLabel.snp.bottom).offset(10)
+            maker.height.leading.trailing.equalTo(player1Label)
+        }
+    }
---
+++ tutorial_assets/ios/tutorial3-rib-di-and-communication/source/source2.swift
@@ -0,0 +1,62 @@
+//
+//  Copyright (c) 2017. Uber Technologies
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import RxSwift
+
+struct Score {
+    let player1Score: Int
+    let player2Score: Int
+
+    static func equals(lhs: Score, rhs: Score) -> Bool {
+        return lhs.player1Score == rhs.player1Score && lhs.player2Score == rhs.player2Score
+    }
+}
+
+protocol ScoreStream: class {
+    var score: Observable<Score> { get }
+}
+
+protocol MutableScoreStream: ScoreStream {
+    func updateScore(withWinner winner: PlayerType)
+}
+
+class ScoreStreamImpl: MutableScoreStream {
+
+    var score: Observable<Score> {
+        return variable
+            .asObservable()
+            .distinctUntilChanged { (lhs: Score, rhs: Score) -> Bool in
+                Score.equals(lhs: lhs, rhs: rhs)
+            }
+    }
+
+    func updateScore(withWinner winner: PlayerType) {
+        let newScore: Score = {
+            let currentScore = variable.value
+            switch winner {
+            case .player1:
+                return Score(player1Score: currentScore.player1Score + 1, player2Score: currentScore.player2Score)
+            case .player2:
+                return Score(player1Score: currentScore.player1Score, player2Score: currentScore.player2Score + 1)
+            }
+        }()
+        variable.value = newScore
+    }
+
+    // MARK: - Private
+
+    private let variable = Variable<Score>(Score(player1Score: 0, player2Score: 0))
+}
