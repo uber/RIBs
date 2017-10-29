0f653a114b93336aa512aa059792d7d551e29c2c
Brian Attwell <attwell@uber.com>
2017-10-29 12:50:34 -0700

    Copy tutorial3 code for memory leaks demo

---
+++ android/demos/memory-leaks/src/test/java/com/uber/rib/root/logged_out/LoggedOutInteractorTest.java
@@ -0,0 +1,49 @@
+package com.uber.rib.root.logged_out;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.uber.rib.core.InteractorHelper;
+import com.uber.rib.core.RibTestBasePlaceholder;
+import io.reactivex.Observable;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class LoggedOutInteractorTest extends RibTestBasePlaceholder {
+
+  @Mock LoggedOutInteractor.Listener listener;
+  @Mock LoggedOutInteractor.LoggedOutPresenter presenter;
+  @Mock LoggedOutRouter router;
+
+  private LoggedOutInteractor interactor;
+
+  @Before
+  public void setup() {
+    MockitoAnnotations.initMocks(this);
+
+    interactor = TestLoggedOutInteractor.create(listener, presenter);
+  }
+
+
+  @Test
+  public void attach_whenViewEmitsName_shouldCallListener() {
+    when(presenter.loginName()).thenReturn(Observable.just("fakename"));
+
+    InteractorHelper.attach(interactor, presenter, router, null);
+
+    verify(listener).login(any(String.class));
+  }
+
+  @Test
+  public void attach_whenViewEmitsEmptyName_shouldNotCallListener() {
+    when(presenter.loginName()).thenReturn(Observable.just(""));
+
+    InteractorHelper.attach(interactor, presenter, router, null);
+
+    verify(listener, never()).login(any(String.class));
+  }
+}
