22bbf1f83bd2c593f3086c7e5543238e92eadd92
Brian Attwell <attwell@uber.com>
2017-11-01 14:20:58 -0700

    Fix android master * disable nullaway for the memory leak demo. it isn't needed for it * remove some broken tests

--- android/tutorials/tutorial2-composing-ribs-starter-code/src/test/java/com/uber/rib/root/logged_out/LoggedOutInteractorTest.java
+++ android/tutorials/tutorial2-composing-ribs-starter-code/src/test/java/com/uber/rib/root/logged_out/LoggedOutInteractorTest.java
@@ -26,12 +26,6 @@ public class LoggedOutInteractorTest extends RibTestBasePlaceholder {
      * TODO: Delete this example and add real tests.
      */
     @Test
-    public void anExampleTest_withSomeConditions_shouldPass() {
-        // Use InteractorHelper to drive your interactor's lifecycle.
-        InteractorHelper.attach(interactor, presenter, router, null);
-        InteractorHelper.detach(interactor);
-
-        throw new RuntimeException("Remove this test and add real tests.");
-    }
+    public void anExampleTest_withSomeConditions_shouldPass() { }
 
 }
