b1d4f010e45e4198d277dbc7c3481bb568bc33f9
Brian Attwell <attwell@uber.com>
2017-11-01 11:02:12 -0700

    More guava licensing
    We need an entry into LICENSES for guava since we directly use some guava source code. Preconditions also needs a  modification copyright date since we have removed some of this code.

--- android/libraries/rib-uava/src/main/java/com/uber/rib/core/Preconditions.java
+++ android/libraries/rib-uava/src/main/java/com/uber/rib/core/Preconditions.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2013 The Guava Authors
+ * Modifications copyright (C) 2017 Uber Technologies, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
