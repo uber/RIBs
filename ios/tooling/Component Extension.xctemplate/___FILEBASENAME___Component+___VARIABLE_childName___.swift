e33d87274ff10632241918e8bc826133253b3093
Cameron Mc Gorian <cameron@sbarow.com>
2017-11-14 09:04:05 +0100

    Add component extension template (#134)

---
+++ ios/tooling/Component Extension.xctemplate/___FILEBASENAME___Component+___VARIABLE_childName___.swift
@@ -0,0 +1,15 @@
+//___FILEHEADER___
+
+import RIBs
+
+/// The dependencies needed from the parent scope of ___VARIABLE_productName___ to provide for the ___VARIABLE_childName___ scope.
+// TODO: Update ___VARIABLE_productName___Dependency protocol to inherit this protocol.
+protocol ___VARIABLE_productName___Dependency___VARIABLE_childName___: Dependency {
+    // TODO: Declare dependencies needed from the parent scope of ___VARIABLE_productName___ to provide dependencies
+    // for the ___VARIABLE_childName___ scope.
+}
+
+extension ___VARIABLE_productName___Component: ___VARIABLE_childName___Dependency {
+
+    // TODO: Implement properties to provide for ___VARIABLE_childName___ scope.
+}
