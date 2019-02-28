496d18e4eec0457781ca1b71a83c369a361b4432
Zsolt Kocsi <zsolt.kocsi@corp.badoo.com>
2019-02-28 18:18:50 +0000

    Simplify method calls

--- android/libraries/rib-workflow/src/main/java/com/uber/rib/workflow/core/WorkflowExtensions.kt
+++ android/libraries/rib-workflow/src/main/java/com/uber/rib/workflow/core/WorkflowExtensions.kt
@@ -1,16 +1,12 @@
 package com.uber.rib.workflow.core
 
-import com.uber.rib.workflow.core.ActionableItem
-import com.uber.rib.workflow.core.Step
 import io.reactivex.Single
 
-fun <A : ActionableItem> stepFrom(f: (() -> A)) =
+fun <A : ActionableItem> stepFrom(f: (() -> A)): Step<Step.NoValue, A> =
     Step.from<Step.NoValue, A>(
-        Single.defer<Step.Data<Step.NoValue, A>> {
-            Single.just<Step.Data<Step.NoValue, A>>(
-                Step.Data.toActionableItem<A>(
-                    f()
-                )
+        Single.fromCallable {
+            Step.Data.toActionableItem<A>(
+                f()
             )
         }
     )
