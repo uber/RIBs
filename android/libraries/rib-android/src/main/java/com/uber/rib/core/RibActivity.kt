ff7654b6db0beddb05f978bbaf05fe7b89c8d164
Zsolt Kocsi <zsolt.kocsi@corp.badoo.com>
2019-03-05 15:43:39 +0000

    Restore RibActivity

---
+++ android/libraries/rib-android/src/main/java/com/uber/rib/core/RibActivity.kt
@@ -0,0 +1,60 @@
+package com.uber.rib.core
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.ViewGroup
+import com.badoo.ribs.core.Node
+
+abstract class RibActivity : AppCompatActivity() {
+
+    private lateinit var rootNode: Node<*>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        rootNode = createRib().apply {
+            onAttach(savedInstanceState)
+            attachToView(rootViewGroup)
+        }
+    }
+
+    abstract val rootViewGroup: ViewGroup
+
+    abstract fun createRib(): Node<*>
+
+    override fun onStart() {
+        super.onStart()
+        rootNode.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        rootNode.onStop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        rootNode.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        rootNode.onResume()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        rootNode.onSaveInstanceState(outState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        rootNode.onDetach()
+        rootNode.detachFromView(findViewById(android.R.id.content))
+    }
+
+    override fun onBackPressed() {
+        if (!rootNode.handleBackPress()) {
+            super.onBackPressed()
+        }
+    }
+}
