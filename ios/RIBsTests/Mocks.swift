14e59721a0556dbdb5b9e29ff2349341af383903
Daisuke Iwata <daimathieu@gmail.com>
2019-05-07 00:17:27 +0900

    fix test mock

--- ios/RIBsTests/Mocks.swift
+++ ios/RIBsTests/Mocks.swift
@@ -15,6 +15,7 @@
 //
 
 import RIBs
+import RxRelay
 import RxSwift
 
 class WindowMock: UIWindow {
@@ -51,18 +52,18 @@ class InteractorMock: Interactable {
         return active.asObservable()
     }
 
-    private let active = Variable<Bool>(false)
+    private let active = BehaviorRelay<Bool>(value: false)
 
     init() {}
 
     // MARK: - Lifecycle
 
     func activate() {
-        active.value = true
+        active.accept(true)
     }
 
     func deactivate() {
-        active.value = false
+        active.accept(false)
     }
 }
 
