3086feffc758b3e973ee9c0df6d9a642f9565dfe
James Barr <jbarr@uber.com>
2021-02-23 11:13:17 -0800

    Extended WorkerBinder functionality to allow for binding a worker to a presenter's lifecycle

--- android/libraries/rib-base/src/main/java/com/uber/rib/core/WorkerBinder.java
+++ android/libraries/rib-base/src/main/java/com/uber/rib/core/WorkerBinder.java
@@ -16,17 +16,13 @@
 package com.uber.rib.core;
 
 import androidx.annotation.VisibleForTesting;
-
 import com.jakewharton.rxrelay2.PublishRelay;
+import com.uber.autodispose.lifecycle.LifecycleScopeProvider;
 import com.uber.rib.core.lifecycle.InteractorEvent;
+import com.uber.rib.core.lifecycle.PresenterEvent;
 import com.uber.rib.core.lifecycle.WorkerEvent;
-
-import java.util.List;
-
 import io.reactivex.Observable;
-import io.reactivex.functions.Consumer;
-import io.reactivex.functions.Function;
-import io.reactivex.functions.Predicate;
+import java.util.List;
 
 /**
  * Helper class to bind to an interactor's lifecycle to translate it to a {@link Worker} lifecycle.
@@ -45,7 +41,7 @@ public final class WorkerBinder {
    * @return {@link WorkerUnbinder} to unbind {@link Worker Worker's} lifecycle.
    */
   public static WorkerUnbinder bind(Interactor<?, ?> interactor, Worker worker) {
-    return bind(interactor.lifecycle(), worker);
+    return bind(mapInteractorLifecycleToWorker(interactor.lifecycle()), worker);
   }
 
   /**
@@ -62,52 +58,106 @@ public final class WorkerBinder {
     }
   }
 
+  /**
+   * Bind a worker (ie. a manager or any other class that needs an presenter's lifecycle) to an
+   * presenter's lifecycle events. Inject this class into your presenter and call this method on any
+   *
+   * @param presenter The presenter that provides the lifecycle.
+   * @param worker The class that wants to be informed when to start and stop doing work.
+   * @return {@link WorkerUnbinder} to unbind {@link Worker Worker's} lifecycle.
+   */
+  public static WorkerUnbinder bind(Presenter presenter, Worker worker) {
+    return bind(mapPresenterLifecycleToWorker(presenter.lifecycle()), worker);
+  }
+
+  /**
+   * Bind a list of workers (ie. a manager or any other class that needs an presenter's lifecycle)
+   * to an presenter's lifecycle events. Use this class into your presenter and call this method on
+   * attach.
+   *
+   * @param presenter The presenter that provides the lifecycle.
+   * @param workers A list of classes that want to be informed when to start and stop doing work.
+   */
+  public static void bind(Presenter presenter, List<? extends Worker> workers) {
+    for (Worker worker : workers) {
+      bind(presenter, worker);
+    }
+  }
+
   @VisibleForTesting
-  static WorkerUnbinder bind(Observable<InteractorEvent> lifecycle, final Worker worker) {
+  static WorkerUnbinder bind(Observable<WorkerEvent> mappedLifecycle, final Worker worker) {
     final PublishRelay<WorkerEvent> unbindSubject = PublishRelay.create();
 
     final Observable<WorkerEvent> workerLifecycle =
-        lifecycle
-            .map(
-                new Function<InteractorEvent, WorkerEvent>() {
-                  @Override
-                  public WorkerEvent apply(InteractorEvent interactorEvent) {
-                    switch (interactorEvent) {
-                      case ACTIVE:
-                        return WorkerEvent.START;
-                      default:
-                        return WorkerEvent.STOP;
-                    }
-                  }
-                })
+        mappedLifecycle
             .mergeWith(unbindSubject)
-            .takeUntil(
-                new Predicate<WorkerEvent>() {
-                  @Override
-                  public boolean test(WorkerEvent workerEvent) throws Exception {
-                    return workerEvent == WorkerEvent.STOP;
-                  }
-                });
+            .takeUntil(workerEvent -> workerEvent == WorkerEvent.STOP);
 
-    workerLifecycle.subscribe(new Consumer<WorkerEvent>() {
-      @Override
-      public void accept(WorkerEvent workerEvent) throws Exception {
-        switch (workerEvent) {
-          case START:
-            worker.onStart(new WorkerScopeProvider(workerLifecycle.hide()));
-            break;
-          default:
-            worker.onStop();
-            break;
-        }
-      }
-    });
+    bindToWorkerLifecycle(workerLifecycle, worker);
 
-    return new WorkerUnbinder() {
-      @Override
-      public void unbind() {
-        unbindSubject.accept(WorkerEvent.STOP);
-      }
-    };
+    return () -> unbindSubject.accept(WorkerEvent.STOP);
+  }
+
+  static Observable<WorkerEvent> mapInteractorLifecycleToWorker(
+      Observable<InteractorEvent> interactorEventObservable) {
+    return interactorEventObservable.map(
+        interactorEvent -> {
+          switch (interactorEvent) {
+            case ACTIVE:
+              return WorkerEvent.START;
+            default:
+              return WorkerEvent.STOP;
+          }
+        });
+  }
+
+  static Observable<WorkerEvent> mapPresenterLifecycleToWorker(
+      Observable<PresenterEvent> presenterEventObservable) {
+    return presenterEventObservable.map(
+        presenterEvent -> {
+          switch (presenterEvent) {
+            case LOADED:
+              return WorkerEvent.START;
+            default:
+              return WorkerEvent.STOP;
+          }
+        });
+  }
+
+  /**
+   * Bind a worker (ie. a manager or any other class that needs an interactor's lifecycle) to an
+   * interactor's lifecycle events.
+   *
+   * @param lifecycle The interactor's {@link LifecycleScopeProvider}.
+   * @param worker The class that wants to be informed when to start and stop doing work.
+   * @deprecated this method uses {@code LifecycleScopeProvider} for purposes other than
+   *     AutoDispose. Usage is strongly discouraged as this method may be removed in the future.
+   */
+  @Deprecated
+  public static void bindTo(
+      LifecycleScopeProvider<InteractorEvent> lifecycle, final Worker worker) {
+    bind(mapInteractorLifecycleToWorker(lifecycle.lifecycle()), worker);
+  }
+
+  /**
+   * Bind a worker to a {@link WorkerEvent} lifecycle provider.
+   *
+   * @param workerLifecycle the worker lifecycle event provider
+   * @param worker the class that wants to be informed when to start and stop doing work
+   */
+  @SuppressWarnings("CheckReturnValue")
+  public static void bindToWorkerLifecycle(
+      Observable<WorkerEvent> workerLifecycle, final Worker worker) {
+    workerLifecycle.subscribe(
+        workerEvent -> {
+          switch (workerEvent) {
+            case START:
+              worker.onStart(new WorkerScopeProvider(workerLifecycle.hide()));
+              break;
+            default:
+              worker.onStop();
+              break;
+          }
+        });
   }
 }
