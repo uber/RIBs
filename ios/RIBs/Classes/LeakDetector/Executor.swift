6d66dd1a7ccf806eec7176324d7fc6ebafb616e1
Pavlo Davydiuk <pavlo.davydiuk@miquido.com>
2021-03-25 17:13:16 +0100

    Bump RxSwift and deployment target (#387)

--- ios/RIBs/Classes/LeakDetector/Executor.swift
+++ ios/RIBs/Classes/LeakDetector/Executor.swift
@@ -38,9 +38,9 @@ public class Executor {
         var didExecute = false
         _ = Observable<Int>
             .timer(DispatchTimeInterval.milliseconds(0), period: period, scheduler: MainScheduler.instance)
-            .takeWhile { _ in
+            .take(while: {  _ in
                 !didExecute
-            }
+            })
             .subscribe(onNext: { _ in
                 let currentTime = Date().timeIntervalSinceReferenceDate
                 let trueElapsedTime = currentTime - lastRunLoopTime
