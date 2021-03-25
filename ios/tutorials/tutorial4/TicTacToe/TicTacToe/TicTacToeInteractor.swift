6c9c04106af414f81500da6ae1d4d5b0aa045cf7
KYHyeon <run190822@gmail.com>
2021-03-26 01:13:32 +0900

    Renames tutorial comments spelling (#386)
    Modifying 'Diagnals' in tutorial comments to 'Diagonal'

--- ios/tutorials/tutorial4/TicTacToe/TicTacToe/TicTacToeInteractor.swift
+++ ios/tutorials/tutorial4/TicTacToe/TicTacToe/TicTacToeInteractor.swift
@@ -145,7 +145,7 @@ final class TicTacToeInteractor: PresentableInteractor<TicTacToePresentable>, Ti
             }
         }
 
-        // Diagnals.
+        // Diagonal.
         guard let p11 = board[1][1] else {
             return nil
         }
