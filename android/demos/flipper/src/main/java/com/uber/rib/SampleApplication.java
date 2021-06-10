a9839723b9091916777c8c8ac96c0a74cf46d9e5
Olivier Notteghem <oliviern@uber.com>
2021-06-10 22:20:27 +0200

    fix spotless errors

--- android/demos/flipper/src/main/java/com/uber/rib/SampleApplication.java
+++ android/demos/flipper/src/main/java/com/uber/rib/SampleApplication.java
@@ -19,23 +19,21 @@ import android.app.Application;
 import com.facebook.flipper.android.AndroidFlipperClient;
 import com.facebook.flipper.android.utils.FlipperUtils;
 import com.facebook.flipper.core.FlipperClient;
-import com.facebook.flipper.plugins.inspector.DescriptorMapping;
-import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin;
 import com.facebook.soloader.SoLoader;
 import com.uber.rib.flipper.BuildConfig;
 import com.uber.rib.flipper.RibTreePlugin;
 
 public class SampleApplication extends Application {
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        SoLoader.init(this, false);
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, false);
 
-        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
-            final FlipperClient client = AndroidFlipperClient.getInstance(this);
-            client.addPlugin(new RibTreePlugin());
-            client.start();
-        }
+    if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
+      final FlipperClient client = AndroidFlipperClient.getInstance(this);
+      client.addPlugin(new RibTreePlugin());
+      client.start();
     }
+  }
 }
