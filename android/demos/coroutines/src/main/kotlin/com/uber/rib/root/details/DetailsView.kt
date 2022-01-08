05fb99019ecfcdc770d0bb106b5bc45f29bd236f
tys <tys@uber.com>
2022-01-07 16:48:38 -0800

    Remvoing coroutines demo folder

--- android/demos/coroutines/src/main/kotlin/com/uber/rib/root/details/DetailsView.kt
+++
@@ -1,8 +0,0 @@
-package com.uber.rib.root.details
-
-import android.content.Context
-import android.util.AttributeSet
-import android.view.View
-import com.uber.rib.root.RootInteractor
-
-class DetailsView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyle: Int = 0) : View(context, attrs, defStyle), RootInteractor.RootPresenter
