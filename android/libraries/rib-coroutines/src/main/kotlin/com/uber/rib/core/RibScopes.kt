05ceab0e8844c859fbd2a03cf44f34524af8536a
tys <tys@uber.com>
2022-04-20 19:58:56 -0700

    Add CoroutineName to scopes

--- android/libraries/rib-coroutines/src/main/kotlin/com/uber/rib/core/RibScopes.kt
+++ android/libraries/rib-coroutines/src/main/kotlin/com/uber/rib/core/RibScopes.kt
@@ -2,24 +2,25 @@ package com.uber.rib.core
 
 import com.uber.autodispose.ScopeProvider
 import com.uber.autodispose.coroutinesinterop.asCoroutineScope
+import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 import java.util.*
 import kotlin.reflect.KProperty
 
 val ScopeProvider.defaultScope by LazyWithReceiver<ScopeProvider, CoroutineScope> {
-    asCoroutineScope(SupervisorJob() + RibDispatchers.Default)
+    asCoroutineScope(SupervisorJob() + CoroutineName("defaultScope") + RibDispatchers.Default)
 }
 
 val ScopeProvider.mainScope by LazyWithReceiver<ScopeProvider, CoroutineScope> {
-    asCoroutineScope(SupervisorJob() + RibDispatchers.Main.immediate)
+    asCoroutineScope(SupervisorJob() + CoroutineName("mainScope") + RibDispatchers.Main.immediate)
 }
 
 val ScopeProvider.ioScope by LazyWithReceiver<ScopeProvider, CoroutineScope> {
-    asCoroutineScope(SupervisorJob() + RibDispatchers.IO)
+    asCoroutineScope(SupervisorJob() + CoroutineName("ioScope") + RibDispatchers.IO)
 }
 val ScopeProvider.unconfinedScope by LazyWithReceiver<ScopeProvider, CoroutineScope> {
-    asCoroutineScope(SupervisorJob() + RibDispatchers.Unconfined)
+    asCoroutineScope(SupervisorJob() + CoroutineName("unconfinedScope") + RibDispatchers.Unconfined)
 }
 
 private class LazyWithReceiver<This,Return>(val initializer:This.()->Return)
