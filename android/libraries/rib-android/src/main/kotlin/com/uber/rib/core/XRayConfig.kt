7574b868e7f5f5b7133b04328bc8831783e8faff
Lucas Lima <me@lucasnlm.dev>
2023-09-06 19:54:15 -0300

    Update UBER License header

--- android/libraries/rib-android/src/main/kotlin/com/uber/rib/core/XRayConfig.kt
+++ android/libraries/rib-android/src/main/kotlin/com/uber/rib/core/XRayConfig.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013. Uber Technologies
+ * Copyright (C) 2023. Uber Technologies
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
